968f9b4721e042d2cc586d7d1c86c375befc2e78
mesode <akwe.mesode@gmail.com>
2023-03-22 19:29:29 -0700

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * prints alphabet in lower case
+ *
+ */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+	}
+
+
+
